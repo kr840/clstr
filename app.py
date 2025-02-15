9c6084d86ca6a1e77f1cedc8970f37f25cd295c0
kr840 <reddykarishma840@gmail.com>
2025-02-15 23:35:53 +0530

    Initial commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from sklearn.neighbors import KNeighborsClassifier 
 
 # Load the pickled model
-with open("Model_KNN", 'rb') as file:
+with open("model_KNN", 'rb') as file:
     loaded_model = pickle.load(file)
 
 st.title('Prediction on Global Development Measurement')
